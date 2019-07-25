52251979e9954789cf748d0b71fd23c1f342776e
Ian Cullinane <ian.cullinane@gmail.com>
2019-07-25 17:01:23 -0400

    rm subnets var

--- variables.tf
+++ variables.tf
@@ -16,10 +16,6 @@ variable "subnet_ids" {
   type        = list(string)
 }
 
-variable "subnets" {
-    description = "List of vpc subnets"
-    type = "string"
-}
 
 variable "log_bucket_name" {
     description = "Name of the S3 bucket for VPC logs"
