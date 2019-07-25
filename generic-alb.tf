884c9f5ce7bb49a1b80eb65ac745232befb51142
Ian Cullinane <ian.cullinane@gmail.com>
2019-07-25 16:47:09 -0400

    initial tf

---
+++ generic-alb.tf
@@ -0,0 +1,58 @@
+
+
+resource "aws_lb" "lb" {
+  name               = "${var.name}"
+  internal           = false
+  load_balancer_type = "${var.type}" 
+  security_groups    = var.security_groups
+  subnets            = ["${var.subnet_ids}"]
+
+  enable_deletion_protection = true
+
+  access_logs {
+    bucket  = "${var.log_bucket_name}"
+    prefix  = "test-lb"
+    enabled = true
+  }
+
+  tags {
+    Name = "${var.name}"
+    environment = "${var.environment}"
+    project = "${var.project}"
+    owner = "${var.owner}"
+  }
+}
+
+# Define a listener
+resource "aws_alb_listener" "generic" {
+  load_balancer_arn = "${aws_alb.lb.arn}"
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    target_group_arn = "${aws_alb_target_group.default_group.arn}"
+    type             = "forward"
+  }
+
+  tags {
+    Name = "${var.name}"
+    environment = "${var.environment}"
+    project = "${var.project}"
+    owner = "${var.owner}"
+  }
+}
+
+
+resource "aws_lb_target_group" "default_group" {
+  name     = "${var.environment}-${var.project}-target-group"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = "${var.vpc_id}"
+
+  tags {
+    Name = "${var.name}"
+    environment = "${var.environment}"
+    project = "${var.project}"
+    owner = "${var.owner}"
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,65 @@
+
+
+variable "prefix" {
+    description = "a prefix for resources to be identified"
+}
+
+variable "vpc_id" {
+    description = "VPC to connect to, used for a security group"
+    type = "string"
+}
+
+variable "security_groups" {
+  description = "A list of security group IDs to associate with"
+  type        = list(string)
+}
+
+variable "subnet_ids" {
+  description = "A list of subnet IDs"
+  type        = list(string)
+}
+
+variable "subnets" {
+    description = "List of vpc subnets"
+    type = "string"
+}
+
+variable "log_bucket_name" {
+    description = "Name of the S3 bucket for VPC logs"
+    type = "string"
+}
+
+variable "type" {
+    description = "LB Type (defaults to 'application'"
+    default = "application"
+}
+
+
+variable "prefix" {
+    description = "a prefix for resources to be identified"
+    default = "${var.environment}-${var.project}"
+}
+
+
+// 
+// Tags 
+//
+
+variable "name" {
+    default = "generic-ec2"
+}
+
+variable "project" {
+    default = "aws-cert"
+}
+
+
+variable "owner" {
+    default = "icullinane"
+}
+
+
+variable "environment" {
+    default = "dev"
+}
+
